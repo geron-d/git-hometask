1bfb19433b07a64f10de0bc46f1af53c213335d2
geron_d <geron_d@mail.ru>
2022-03-27 16:12:56 +0300

    fixed method 2 and 3

--- src/main/java/by/it/academy/square/triangle/TriangleSquareImplementation.java
+++ src/main/java/by/it/academy/square/triangle/TriangleSquareImplementation.java
@@ -9,13 +9,14 @@ public class TriangleSquareImplementation implements TriangleSquare{
 
     @Override
     public double squareThroughTwoSidesAndAngleBetweenThem(double sideOne, double sideTwo, double angle) {
-        double square = 0.5 * sideOne * sideTwo * Math.sin(angle);
+        double square = 0.5 * sideOne * sideTwo * Math.sin(Math.toRadians(angle));
         return square;
     }
 
     @Override
     public double squareThroughSidesAndTwoAdjacentAngles(double side, double angleOne, double angleTwo) {
-        double square = Math.pow(side, 2) * (Math.sin(angleOne) * Math.sin(angleTwo)) / 2 / Math.sin(angleOne + angleTwo);
+        double square = Math.pow(side, 2) * (Math.sin(Math.toRadians(angleOne)) * Math.sin(Math.toRadians(angleTwo)))
+                / 2 / Math.sin(Math.toRadians(angleOne) + Math.toRadians(angleTwo));
         return square;
     }
 }
