788d52e76468e0463a96c82d4d47dc63deeaae3e
geron_d <geron_d@mail.ru>
2022-03-27 16:33:51 +0300

    add rightTriangleSquare

--- src/main/java/by/it/academy/App.java
+++ src/main/java/by/it/academy/App.java
@@ -1,6 +1,8 @@
 package by.it.academy;
 
 
+import by.it.academy.square.triangle.RightTriangleSquare;
+import by.it.academy.square.triangle.RightTriangleSquareImplementation;
 import by.it.academy.square.triangle.TriangleSquare;
 import by.it.academy.square.triangle.TriangleSquareImplementation;
 
@@ -12,5 +14,11 @@ public class App
         System.out.println("squareThroughBaseAndHeight : " + triangleSquare.squareThroughBaseAndHeight(5, 5));
         System.out.println("squareThroughTwoSidesAndAngleBetweenThem : " + triangleSquare.squareThroughTwoSidesAndAngleBetweenThem(5, 5, 90));
         System.out.println("squareThroughSidesAndTwoAdjacentAngles : " + triangleSquare.squareThroughSidesAndTwoAdjacentAngles(5, 90, 45));
+        System.out.println();
+
+        RightTriangleSquare rightTriangleSquare = new RightTriangleSquareImplementation();
+        System.out.println("squareThroughTwoLegs : " + rightTriangleSquare.squareThroughTwoLegs(5, 5));
+        System.out.println("squareThroughHypotenuseAndSharpAngle : " + rightTriangleSquare.squareThroughHypotenuseAndSharpAngle(7, 45));
+        System.out.println("squareThroughLegAndAdjacentAngles : " + rightTriangleSquare.squareThroughLegAndAdjacentAngles(5, 45));
     }
 }
