71eb005705d70620e43f5da362a6fde1823f8b4f
geron_d <geron_d@mail.ru>
2022-03-27 16:07:08 +0300

    add test - method 2 and 3 doesn't work true

--- src/main/java/by/it/academy/App.java
+++ src/main/java/by/it/academy/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-
+        TriangleSquare triangleSquare = new TriangleSquareImplementation();
+        System.out.println("squareThroughBaseAndHeight : " + triangleSquare.squareThroughBaseAndHeight(5, 5));
+        System.out.println("squareThroughTwoSidesAndAngleBetweenThem : " + triangleSquare.squareThroughTwoSidesAndAngleBetweenThem(5, 5, 90));
+        System.out.println("squareThroughSidesAndTwoAdjacentAngles : " + triangleSquare.squareThroughSidesAndTwoAdjacentAngles(5, 90, 45));
     }
 }
